7ec29648dd0599650e240382b8d5bdf3d30d6bf6
Bruno Verçosa <69854207+brunoverc@users.noreply.github.com>
2023-03-15 12:20:57 -0300

    Tabela Livros init

--- tabela-livros/src/App.js
+++ tabela-livros/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component}  from "react";
+import TabelaHead from "./components/TabelaHead";
+import TabelaBody from "./components/TabelaBody";
+import TabelaFoot from "./components/TabelaFoot";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    livros : [
+      {
+        id : "978-85-7522-632-2",
+        titulo: "CSS Grid Layout",
+        autor: "Maurício Samy Silva"
+      },
+      {
+        id : "978-85-7522-632-3",
+        titulo: "Node Essencial",
+        autor: "Ricardo R. Lecheta"
+      },
+      {
+        id : "978-85-7522-632-7",
+        titulo: "Aprendendo Material Design",
+        autor: "Kyle New"
+      },
+    ]
+  }
+  render(){
+    return (
+      <table className="tabela">
+        <TabelaHead />
+        <TabelaFoot />
+        <TabelaBody livros={this.state.livros}/>
+  
+      </table>
+    );
+  }
 }
 
 export default App;
