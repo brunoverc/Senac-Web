8a6cc9dac5160f95806786c92844013c07304a7f
brunoverc <69854207+brunoverc@users.noreply.github.com>
2023-03-28 16:51:32 -0300

    Componentes

--- editora-senac/src/App.js
+++ editora-senac/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Topo from "./components/Topo";
+import Principal from "./components/Principal";
+import Rodape from "./components/Rodape";
 
 const App = () => {
   return(
-    <Topo />
+    <>
+      <Topo />
+      <Principal />
+      <Rodape />
+    </>
   ); 
 };
 
--- editora-senac/src/components/Topo.js
+++ editora-senac/src/components/Topo.js
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "./Logo";
 import Navegacao from "./Navegacao";
 
+
 const Topo = () =>{
     return(
         <header className="topo">
