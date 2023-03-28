aa4ece8dc71ec2bb767983c974fd44bb4576b0c1
brunoverc <69854207+brunoverc@users.noreply.github.com>
2023-03-28 14:42:36 -0300

    Navegacao

---
+++ editora-senac/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Topo from "./components/Topo";
+
+const App = () => {
+  return(
+    <Topo />
+  ); 
+};
+
+export default App;
--- tabela-livros/src/App.js
+++ tabela-livros/src/App.js
@@ -1,34 +1,56 @@
 import React, {Component}  from "react";
+
 import TabelaHead from "./components/TabelaHead";
 import TabelaBody from "./components/TabelaBody";
 import TabelaFoot from "./components/TabelaFoot";
 
 class App extends Component{
   state = {
-    livros : [
-      {
-        id : "978-85-7522-632-2",
-        titulo: "CSS Grid Layout",
-        autor: "Maurício Samy Silva"
-      },
-      {
-        id : "978-85-7522-632-3",
-        titulo: "Node Essencial",
-        autor: "Ricardo R. Lecheta"
-      },
-      {
-        id : "978-85-7522-632-7",
-        titulo: "Aprendendo Material Design",
-        autor: "Kyle New"
-      },
-    ]
+    livros : []
+  };
+
+  componentDidMount(){
+    fetch("api/livros.json")
+    .then(response => response.json())
+    .then(livros => this.setState({livros}))
+    .catch(function(error){
+      console.log("Erro na requisição");
+    })
+    .finally(function(){
+      console.log("Sempre retorna");
+    })
   }
+  handleRemoverLinha = (id) => {
+    const livros = this.state.livros.filter(livro => livro.id !== id);
+    this.setState({livros});
+  };
+
+  handleOrdernarCrescente = (titulo) =>{
+    const livros = this.state.livros.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+      );
+      this.setState({livros});
+  };
+  handreOrdernarDecrescente = (titulo) => {
+    const livros = this.state.livros.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    );
+    livros.reverse();
+    this.setState({livros});
+  };
+
   render(){
     return (
       <table className="tabela">
-        <TabelaHead />
-        <TabelaFoot />
-        <TabelaBody livros={this.state.livros}/>
+        <TabelaHead 
+          ordernarCrescente = {this.handleOrdernarCrescente}
+          ordenarDecrescente = {this.handreOrdernarDecrescente}
+        />
+        <TabelaFoot qtdeLivros={this.state.livros.length}/>
+        <TabelaBody 
+        livros={this.state.livros}
+        removerLinha={this.handleRemoverLinha}
+        />
   
       </table>
     );
