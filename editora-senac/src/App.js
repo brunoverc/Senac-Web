e674f72e322579914364e6ac645261a213264bcb
brunoverc <69854207+brunoverc@users.noreply.github.com>
2023-04-11 16:33:10 -0300

    Read json

--- editora-senac/src/App.js
+++ editora-senac/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topo from "./components/Topo";
 import Principal from "./components/Principal";
@@ -11,20 +11,55 @@ import Catalogo from "./components/Catalogo";
 import NotFound from "./components/NotFound";
 import Home from "./components/Home";
 
-class App extends Component{
+import Livro from "./components/Livro"
+import axios from "axios"
+
+class App extends Component {
+  state = {
+    livros: []
+  };
+
+  async componentDidMount() {
+    try {
+      //sintaxe para requisição do axios
+      const { data: livros } = await axios.get("/api/todosOsLivros.json");
+      //Objeto contendo os dados vindos da requisição sendo armazenados no objeto livros e 
+      //em seguida esse objeto é passado para o método setState()
+      this.setState({ livros });
+    }
+    catch (error) {
+      console.log(error);
+      document.querySelectorAll(".principal")[0]
+        .insertAdjacentHTML(
+          "beforeend",
+          "<p class='erro'>Ops! Ocorreu um erro </p>"
+        )
+    }
+  }
+
   render() {
     return (
       //O componente Router deve ser o container geral da aplicação
-      <Router> 
+      <Router>
         <>
           <Topo />
           <Routes>
-            <Route exact path="/" element={<Home />}/>
-            <Route exact path="/frontend" element={<Frontend />}/>
-            <Route exact path="/programacao" element={<Programacao />}/>
-            <Route exact path="/design" element={<Design />}/>
-            <Route exact path="/catalogo" element={<Catalogo />}/>
-            <Route path="*" element={<NotFound/>}/>
+            <Route exact path="/" element={<Home livros={this.state.livros} />} />
+
+            <Route exact path="/frontend" element={<Frontend livros={this.state.livros} />} />
+            <Route exact path="/programacao" element={<Programacao livros={this.state.livros} />} />
+            <Route exact path="/design" element={<Design livros={this.state.livros} />} />
+            //teste
+                        
+            <Route exact path="/catalogo" element={<Catalogo livros={this.state.livros} />} />
+            <Route path="/livro/:livroSlug" render={props => {
+              const livro = this.state.livros.find(
+                livro => livro.slug === props.match.params.livroSlug);
+              if (livro) return <Livro livro={livro} />;
+              else return <NotFound />;
+            }}
+            />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </>
       </Router>
--- editora-senac/src/components/Home.js
+++ editora-senac/src/components/Home.js
@@ -1,12 +1,43 @@
 import React from "react";
-
-const Home = () => (
+import { Link } from "react-router-dom";
+const Home = ({ livros }) => ( //O objeto {livros} passado do componente App e recebido por props
     <main className="principal">
         <h2>Últimos lançamentos</h2>
-        <div className="card">
-            <p>Conteúdos da Home Page</p>
-        </div>
+        {livros
+            //O método filter() aplicado ao array de dados recebido separa e 
+            //monta um array com os seis primeiros livros do banco de dados
+            .filter((n, index) => index < 6)
+            //O método map() é aplicado ao objeto de dados dos seis livros e 
+            //gera a marcação HTML para a Home Page
+            .map(livro => (
+                //E React para toda lista gerada dinamicamente, é mandatório que 
+                //os itens da lista recebam o atributo key, com um valor único.
+                <div className="card" key={livro.id}>
+                    <div className="thumb">
+                        <img
+                            src={"/imagens/capas/" + livro.isbn.replace(/-/g, "") + ".jpg"}
+                            alt="thumbnail da capa do livro ..."/>
+                    </div>
+                    {livros
+                        //O método filter() cria um array de slugs dos livros
+                        .filter(c => c.slug === livro.slug)
+                        //Mapeamos o array filtrado pelo slug
+                        .map(livro => (
+                            //O componente Link da biblioteca React Router é o conteiner
+                            //para todos os detalhes do livro. Ele "transforma" todos os
+                            //detalhes em um link para a página do livro
+                            <Link to={`\livro\${livro.slug}`} key={livro.id}>
+                                {
+                                    <div className="detalhes">
+                                        <h3>{livro.titulo}</h3>
+                                        <p>{livro.descricao.slice(0,130) + "..."}</p>
+                                        <p>Leia mais &gt;</p>
+                                    </div>
+                                }
+                            </Link>
+                        ))}
+                </div>
+            ))}
     </main>
 );
-
 export default Home;
